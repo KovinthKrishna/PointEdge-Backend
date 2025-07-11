5986d6a0243d00d72db7aaa7f41984dfa1908614
Kovintharajan Krishnasuntharam <KovinthKrishna@gmail.com>
2025-07-11 18:26:13 +0530

    Bug fix

--- src/main/java/com/eternalcoders/pointedge/security/JwtUtil.java
+++ src/main/java/com/eternalcoders/pointedge/security/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.eternalcoders.pointedge.security;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
