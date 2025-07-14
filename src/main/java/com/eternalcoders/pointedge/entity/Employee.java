9534e84562e9cbccf31cdba89843bbfb845067a1
Thinura Senitha Kahaduwa <thinurakahaduwa@gmail.com>
2025-07-14 19:12:01 +0530

    employees page backend updated

--- src/main/java/com/eternalcoders/pointedge/entity/Employee.java
+++ src/main/java/com/eternalcoders/pointedge/entity/Employee.java
@@ -19,6 +19,12 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     // Personal Info
     @Column(nullable = false)
     private String name; // full name
